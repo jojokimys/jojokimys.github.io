35cf52a077a09b9ab682a3867a4475579ec58b2b
jojokimys <jojokimys@users.noreply.github.com>
2020-10-31 05:11:03 +0900

    lab7 - y label fix

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -41,7 +41,7 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurants By Category',
       labelFontSize: 12,
       scaleBreaks: {
         customBreaks: [
